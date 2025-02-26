8bb48c4cba16412a7b7cc33728e663803497d7c0
김종연 <bellopenerr@gmail.com>
2025-02-26 11:55:23 +0900

    레벨 2: TodoRepository 수정

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -1,23 +1,23 @@
 package org.example.expert.domain.todo.repository;
 
+import java.util.Optional;
 import org.example.expert.domain.todo.entity.Todo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.Optional;
-
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
-    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
-    Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
+  @EntityGraph(attributePaths = "user")
+  @Query("SELECT DISTINCT t FROM Todo t")
+  Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
 
-    @Query("SELECT t FROM Todo t " +
-            "LEFT JOIN FETCH t.user " +
-            "WHERE t.id = :todoId")
-    Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
+  @EntityGraph(attributePaths = "user")
+  @Query("SELECT DISTINCT t FROM Todo t WHERE t.id = :todoId")
+  Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
 
-    int countById(Long todoId);
+  int countById(Long todoId);
 }
